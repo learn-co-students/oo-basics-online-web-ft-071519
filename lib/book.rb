aae509de7245f19eadc56c3abccd4fa8b494c655
Audrea Cook <audreacwork@gmail.com>
2019-07-19 13:17:44 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
+
+### prediction
+ # book = Book.new("Harry Potter") 
+ # puts book.title #=> "Harry Potter"
+ # book.author = "J.K. Rowling"
+ # puts book.author
+ # book.page_count = 4224
+ # puts book.page_count #=> 4224
+ # book.genre = "fantasy"
+ # puts book.genre #=> "fantasy"
+ # book.turn_page #=> "Flipping the page...wow, you read fast!"
+
 
 
